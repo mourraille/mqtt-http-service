f0a5549fdbf6af3a6b8d2097a2f389132c035327
Mauricio Mourraille <mourraille@me.com>
2019-07-22 23:18:59 +0200

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const mqtt = require('mqtt');
 const https = require('https');
+var nodemailer = require('nodemailer');
 
 var client  = mqtt.connect("mqtt://raspberrypi", null);
 client.on("connect",function() {	
-console.log("connected");
 client.subscribe("mailbox");
 });
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+           user: 'mauricio.mourraille@ucrso.info',
+           pass: ''
+       }
+   });
+
+   const mailOptions = {
+    from: 'mauricio.mourraille@ucrso.info', // sender address
+    to: 'mourraille@me.com,knajjars@gmail.com', // list of receivers
+    subject: "You've got mail! ", // Subject line
+    html: '<p>You' +'\'ve got mail 📩 !    -> ' + new Date() +'</p>'// plain text body
+  };
 
 client.on('message',function(topic, message, packet) {
-    console.log("You've got mail 📩");
-    https.get('', (data) => {
-    })
+    console.log("You've got mail 📩     ->" + new Date());
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info);
+     });
+  //  https.get('', (data) => {
+    //})
 });
 
 
